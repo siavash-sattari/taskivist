a9debc09a83f65d1cdecb2ae62a0d9388f5fab75
siavash sattari <siavash.sattari.dev@gmail.com>
2023-04-11 23:40:32 +0330

    chore minor changes

--- src/components/icons/Menu.tsx
+++ src/components/icons/Menu.tsx
@@ -2,8 +2,8 @@ import { SVGProps } from 'react';
 
 const MenuIcon = (props: SVGProps<SVGSVGElement>) => {
   return (
-    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' stroke-width='1.5' stroke='currentColor' className='w-6 h-6' {...props}>
-      <path stroke-linecap='round' stroke-linejoin='round' d='M3.75 9h16.5m-16.5 6.75h16.5' />
+    <svg xmlns='http://www.w3.org/2000/svg' fill='none' viewBox='0 0 24 24' strokeWidth='1.5' stroke='currentColor' className='w-6 h-6' {...props}>
+      <path strokeLinecap='round' strokeLinejoin='round' d='M3.75 9h16.5m-16.5 6.75h16.5' />
     </svg>
   );
 };
